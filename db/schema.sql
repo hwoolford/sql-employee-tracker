deb63eb500d09947685f7eb131586bcc40820be1
forxtoisate <hwoolford@gmail.com>
2023-12-19 17:20:49 -0600

    Added primary keys

--- db/schema.sql
+++ db/schema.sql
@@ -4,29 +4,26 @@ CREATE DATABASE tracker_db;
 USE tracker_db;
 
 CREATE TABLE department (
-    id INT,
+    id INT PRIMARY KEY,
     name VARCHAR(30),
-    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT,
+    id INT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    PRIMARY KEY (id),
     FOREIGN KEY (department_id),
     REFERENCES department(id),
     ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT,
+    id INT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    PRIMARY KEY (id),
     FOREIGN KEY (role_id),
     REFERENCES role(id),
     ON DELETE SET NULL
