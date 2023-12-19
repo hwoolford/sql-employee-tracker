0a772cfc5d85c02492ed43a40ebaa2772885ad14
forxtoisate <hwoolford@gmail.com>
2023-12-19 13:29:13 -0600

    Added database and tables to schema.sql

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mysql = require('mysql2');
+const inquirer = require("inquirer");
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static("public"));
+
+
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
