716845424c7eb971ba5cb9f791eba1642c0dd53e
forxtoisate <hwoolford@gmail.com>
2024-01-11 11:39:57 -0600

    Added missing comma in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -22,7 +22,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
     FOREIGN KEY (manager_id) REFERENCES employee(id)
     
 );
